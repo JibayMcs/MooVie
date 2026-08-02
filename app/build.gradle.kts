48a38c16296a6d9fefd39d246f5189b16c2b70fe
JibayMcs <theamateis@gmail.com>
2026-08-02 17:38:36 +0200

    chore(release): Moo-vie v1.10.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -13,7 +13,7 @@ plugins {
 
 // Version unique Android + desktop (l'updater compare les tags GitHub à cette
 // valeur ; côté desktop elle est injectée en propriété système moovie.version).
-val appVersion = "1.9.0"
+val appVersion = "1.10.0"
 
 // Signature release : keystore.properties en local (gitignoré), variables
 // d'environnement en CI (KEYSTORE_FILE / KEYSTORE_PASSWORD / KEY_ALIAS).
@@ -141,7 +141,7 @@ android {
         applicationId = "fr.moovie.tv"
         minSdk = 23
         targetSdk = 34
-        versionCode = 20
+        versionCode = 21
         versionName = appVersion
     }
 
